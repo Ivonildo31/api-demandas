00cb2d736194a502c02f5de428791d906835f967
vizeke <vizeke@gmail.com>
2017-07-19 19:23:17 -0300

    Utiliza horário local do feedback

--- server/services/ceturb-payload.ts
+++ server/services/ceturb-payload.ts
@@ -31,12 +31,12 @@ const getUser = ( user: any ) => {
 export let ceturbPayload = {
   getEmailBody ( demand: IDemand ) {
     let payload = demand.payload
-    let mailBody = `Data de envio: ${moment( demand.createdAt ).format( 'DD/MM/YYYY HH:mm:ss' )}\n`
+    let mailBody = `Data de envio: ${moment( demand.createdAt ).local().format( 'DD/MM/YYYY HH:mm:ss' )}\n`
     mailBody += `tipo: ${getTypeDemand( payload.type )}\n`
 
     // Ajusta a data do horário informado, pois no formulário ele preenche a data como 1/1/1970.
     // TODO: Por enquanto utilizando a data de criação. Mas deve ser ajustado no formulário do aplicativo.
-    mailBody += payload.date ? `hora: ${moment( demand.createdAt.substring( 0, 11 ) + payload.date.substring( 11 ) ).format( 'HH:mm' )}\n` : ''
+    mailBody += payload.date ? `hora: ${moment( demand.createdAt.substring( 0, 11 ) + payload.date.substring( 11 ) ).local().format( 'HH:mm' )}\n` : ''
 
     mailBody += payload.line ? `linha: ${payload.line}\n` : ''
     mailBody += payload.stop ? `ponto: ${payload.stop}\n` : ''
